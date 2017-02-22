93d075d26f7da7838f6b0984c546e53bf20bcfbc
Thomas <thomas.grilletta@gmail.com>
2017-02-22 14:16:24 +0100

    Clean provided names

--- src/main/java/com/exabilan/types/exalang/Person.java
+++ src/main/java/com/exabilan/types/exalang/Person.java
@@ -1,5 +1,7 @@
 package com.exabilan.types.exalang;
 
+import static org.apache.commons.lang3.text.WordUtils.capitalize;
+
 import lombok.Value;
 import lombok.experimental.NonFinal;
 
@@ -8,8 +10,16 @@ public class Person {
     String firstName;
     String lastName;
 
+    public String getFirstName() {
+        return capitalize(firstName.toLowerCase());
+    }
+
+    public String getLastName() {
+        return lastName.toUpperCase();
+    }
+
     public String getFullName() {
-        return firstName + " " + lastName.toUpperCase();
+        return getFirstName() + " " + getLastName();
     }
 
 }
